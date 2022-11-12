13a1255c6361a2d154d397dc5062133842f36325
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-11-12 13:16:28 +0100

    gauge let's go

--- src/main/java/com/hephaestus/businessdemo/MetricExporter.java
+++ src/main/java/com/hephaestus/businessdemo/MetricExporter.java
@@ -1,7 +1,7 @@
 package com.hephaestus.businessdemo;
 
 import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Histogram;
+import io.prometheus.client.Gauge;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,9 +10,7 @@ import java.util.Random;
 
 @Component
 public class MetricExporter {
-
-    private int criticalFactor = 0;
-    private final Histogram histogram;
+    private final Gauge gauge;
     private final Random random = new Random();
     private final String pod;
     private final String namespace;
@@ -20,7 +18,7 @@ public class MetricExporter {
     public MetricExporter(CollectorRegistry registry, @Value("${pod}") String pod, @Value("${namespace}") String namespace) {
         this.pod = pod;
         this.namespace = namespace;
-        histogram = Histogram.build()
+        gauge = Gauge.build()
                 .help("critical metric")
                 .name("critical_metric")
                 .labelNames("pod", "namespace")
@@ -29,8 +27,6 @@ public class MetricExporter {
 
     @Scheduled(fixedRate = 1000)
     private void runIncrementer() {
-            criticalFactor += random.nextInt(5);
-            System.out.println(criticalFactor);
-            histogram.labels(pod, namespace).observe(criticalFactor);
+            gauge.labels(pod, namespace).inc(random.nextInt(5));
     }
 }
